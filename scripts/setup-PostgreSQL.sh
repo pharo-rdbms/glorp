375dac5dacf70d6e671e408ed850605048c349ff
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2022-04-13 14:16:44 -0300

    Update script and badges

--- scripts/setup-PostgreSQL.sh
+++ scripts/setup-PostgreSQL.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-readonly DOCKER_IMAGE_VERSION=$(echo "$RDBMS" | cut --complement -c -11)
+DOCKER_IMAGE_VERSION=$(echo "$RDBMS" | cut --complement -c -11)
 docker run -d -p 127.0.0.1:5432:5432 \
   -e POSTGRES_PASSWORD=secret \
   -e POSTGRES_USER=postgres \
