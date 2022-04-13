1b8f688a77c9b2a162d105c575b118c1f8584fde
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2022-04-13 13:53:12 -0300

    Rollback script change

--- scripts/setup-PostgreSQL.sh
+++ scripts/setup-PostgreSQL.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-readonly DOCKER_IMAGE_VERSION
-DOCKER_IMAGE_VERSION=$(echo "$RDBMS" | cut --complement -c -11)
+readonly DOCKER_IMAGE_VERSION=$(echo "$RDBMS" | cut --complement -c -11)
 docker run -d -p 127.0.0.1:5432:5432 \
   -e POSTGRES_PASSWORD=secret \
   -e POSTGRES_USER=postgres \
